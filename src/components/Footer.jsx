5b6aa691f0fe7a076335c5850a6d518f74299d60
Neyu <eaa25nak@students.eaaa.dk>
2026-03-02 17:47:23 +0100

    styled footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,30 @@
+import './Footer.css'
+
+
+// This code uses <span> to separate the words considered "labels" away from the rest of the code. this allows me to do some cute coloring with them :)
+
 export default function Footer() {
 
     return (
         <footer className="footer">
-            <h2>Info:</h2>
-            <h3>Contact information:</h3>
-            <h3>Address: 123 Botanical Lane, Green City, 2345 Copenhagen, Denmark</h3>
-            <h3>Email: flowplant123@flowerplant2026.dk</h3>
-            <h3>Mobile: +452076765</h3>
+            <div className="main">
+                <h3 className="title">Contact information:</h3>
+
+                
+                <p className="para">
+                    <span className="label">Address:</span> 123 Botanical Lane, Green City, 2345 Copenhagen, Denmark
+                </p>
+                <p className="para">
+                    <span className="label">Email:</span> flowplant123@flowerplant2026.dk
+                </p>
+                <p className="para">
+                    <span className="label">Mobile:</span> +452076765
+                </p>
+
+                <p className="small">
+                    © 2026 FlowerPlant. All rights reserved.
+                </p>
+            </div>
         </footer>
-    )
+    );
 }
